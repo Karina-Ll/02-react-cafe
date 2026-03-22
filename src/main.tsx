fbb9a775763e1c6a94ae50b1306d014f818b5af9
Karina <karina24032006.09@gmail.com>
2026-03-23 00:43:08 +0200

    Fix dependencies and paths for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize'; // Важливо для стилів!
-import App from './components/App/App';
+import 'modern-normalize';
+import App from './components/App/App'; // Перевір, чи папки називаються App/App.tsx
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
